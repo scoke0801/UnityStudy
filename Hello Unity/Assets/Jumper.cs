1be8eaadb0df72ce724cd95f8ba53d6de9e8058e
LJH <scoke0801@gmail.com>
2022-04-01 21:39:59 +0900

    component test

---
+++ Hello Unity/Assets/Jumper.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jumper : MonoBehaviour
+{
+    public Rigidbody myRigidBody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidBody.AddForce(0, 500, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

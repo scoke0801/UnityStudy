600657b2eaa080b374455a214003c1da3e112237
LJH <scoke0801@gmail.com>
2022-04-04 21:16:36 +0900

    소코반 맵 디자인 까지

--- Sokoban/Assets/Player.cs
+++ Sokoban/Assets/Player.cs
@@ -3,23 +3,35 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
-{ 
-    float speed = 10.0f;
+{
+    private float speed = 10.0f;
     
-    public Rigidbody playerRigidBody;
+    private Rigidbody playerRigidBody;
 
     // 게임이 처음 시작되었을 때 한 번
     void Start()
     {
-        playerRigidBody.AddForce(0, 1000, 0);
+        // AddForce :: 힘을 주고, 주어진 힘으로 내부에서 속도를 다시 계산
+        //playerRigidBody.AddForce(0, 1000, 0);
+        playerRigidBody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.W))
-        {
-            playerRigidBody.AddForce(0, 0, speed);
-        }
+        // -1.0 ~ 1.0
+        // A, <- 키 : -1.0
+        // 아무것도 안누르면 : 0.0
+        // D, -> 키 : 1.0
+        // 조이스틱을 고려하여 float값으로 반환
+        float inputX = Input.GetAxis("Horizontal");
+        float inputZ = Input.GetAxis("Vertical");
+         
+        float fallSpeed = playerRigidBody.velocity.y;
+        Vector3 velocity = new Vector3(inputX, 0.0f, inputZ);
+        velocity = velocity * speed;
+        velocity.y = fallSpeed;
+
+        playerRigidBody.velocity = velocity;
     }
 }

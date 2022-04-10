965be9bdeaf7029dc68154b56424137084537764
LJH <scoke0801@gmail.com>
2022-04-10 13:13:28 +0900

    어메이징 볼링 ~ 프롭

--- AmazingBowling/Assets/Scenes/Scripts/Ball.cs
+++ AmazingBowling/Assets/Scenes/Scripts/Ball.cs
@@ -15,6 +15,8 @@ public class Ball : MonoBehaviour
     public float lifeTime = 10.0f;
 
     public float explosionRadius = 20.0f;
+
+    public LayerMask whatIsProp;
     // Start is called before the first frame update
     void Start()
     {
@@ -29,6 +31,21 @@ public class Ball : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
+        // 인자 범위 안에 해당하는 가상의 구를 그려서 해당하는 오브젝트를 배열로 반환
+        Collider[] colliders = Physics.OverlapSphere(transform.position, explosionRadius, whatIsProp);
+        
+        for (int i = 0; i < colliders.Length; ++i)
+        {
+            Rigidbody targetRigidbody = colliders[i].GetComponent<Rigidbody>();
+
+            targetRigidbody.AddExplosionForce(explosionForce, transform.position, explosionRadius);
+
+            Prop targetProp = colliders[i].GetComponent<Prop>();
+
+            float damage = CalculateDamage(targetProp.transform.position);
+
+            targetProp.TakeDamage(damage);
+        }
         // 부모 해제
         // 아래에서 object를 삭제할 때, 사라지지 않도록
         explosionParticle.transform.parent = null; 
@@ -37,5 +54,23 @@ public class Ball : MonoBehaviour
 
         Destroy(explosionParticle, explosionParticle.duration);
         Destroy(gameObject);
+    }
+
+    private float CalculateDamage(Vector3 targetPosition)
+    {
+        Vector3 explositionToTarget = targetPosition - transform.position;
+
+        // 거리
+        float distance = explositionToTarget.magnitude;
+
+        float edgeToCenterDistance = explosionRadius - distance;
+
+        float percentage = edgeToCenterDistance / explosionRadius;
+
+        float damage = maxDamage * percentage;
+        
+        damage = Mathf.Max(damage, 0);
+
+        return damage;
     }
 }
---
+++ AmazingBowling/Assets/Scenes/Scripts/Prop.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Prop : MonoBehaviour
+{
+    public int score = 5;
+    public ParticleSystem explosionPartice;
+
+    public float hp = 10.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void TakeDamage(float damaga)
+    {
+        hp -= damaga;
+        if (hp <= 0)
+        {
+            // 원본 게임 오브젝트를 집어넣으면 복사해줌
+            ParticleSystem instance = Instantiate(explosionPartice, transform.position, transform.rotation);
+            instance.Play();
+            Destroy(instance.gameObject, instance.duration);
+
+            AudioSource audioSource = instance.GetComponent<AudioSource>();
+            audioSource.Play();
+            // Prop은 파괴보단 재활용
+            gameObject.SetActive(false);
+        }
+    }
+}

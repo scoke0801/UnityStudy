029716e38febaf2160191ede7a1f4adc544eeecf
Lim Jong Hyun <scoke0801@gmail.com>
2022-09-19 23:18:15 +0900

    노트북 코드 저장용

---
+++ QuestSystem/Assets/Scripts/UI/Quest Tracker/TaskDescriptor.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TaskDescriptor : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI _text;
+
+    private string BuildText(Task task, string textColorCode, string successCountColorCode)
+    {
+        return $"<color="
+    }
+}

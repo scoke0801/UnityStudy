11d3a8eaff93010736401574dbd971577ab2dedf
LJH <scoke0801@gmail.com>
2022-09-25 00:42:34 +0900

    메인 퀘스트 -2
    To Way out

--- QuestSystem/Assets/Creator Kit - FPS/Scripts/System/EndCheckpoint.cs
+++ QuestSystem/Assets/Creator Kit - FPS/Scripts/System/EndCheckpoint.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class EndCheckpoint : MonoBehaviour
 {
+    [SerializeField]
+    private QuestReporter questReporter;
+
     void OnTriggerEnter(Collider other)
     {
         if (other.GetComponent<Controller>() == null)
             return;
-        
+
+        questReporter.Report();
         
         GameSystem.Instance.StopTimer();
         GameSystem.Instance.FinishRun();

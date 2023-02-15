c26cadf94e30852850448d9b72337fcae5aab543
scoke0801 <scoke0801@gmail.com>
2023-02-15 22:58:14 +0900

    collision - spark particle

--- FPSStudy/Assets/02.Scripts/RemoveBullet.cs
+++ FPSStudy/Assets/02.Scripts/RemoveBullet.cs
@@ -4,12 +4,19 @@ using UnityEngine;
 
 public class RemoveBullet : MonoBehaviour
 {
+    public GameObject sparkEffect;
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.collider.CompareTag( "BULLET"))
         {
+            ContactPoint cp = collision.GetContact(0);
+
+            Quaternion rot = Quaternion.LookRotation(-cp.normal);
+
+            GameObject spark = Instantiate(sparkEffect, cp.point, rot);
+            Destroy(spark, 0.5f);
+
             Destroy(collision.gameObject);
         }
     }
-
 }

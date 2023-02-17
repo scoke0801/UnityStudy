ed092178ecbe79b116b9552dba2ce01ab65f0410
scoke0801 <scoke0801@gmail.com>
2023-02-17 22:36:59 +0900

    Bullet sounds

--- FPSStudy/Assets/02.Scripts/FireCtrl.cs
+++ FPSStudy/Assets/02.Scripts/FireCtrl.cs
@@ -3,11 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
+[RequireComponent(typeof(AudioSource))]
 public class FireCtrl : MonoBehaviour
 {
     public GameObject bullet;
 
     public Transform firePos;
+
+    public AudioClip fireSfx;
+
+    private new AudioSource audio;
+
+    private void Start()
+    {
+        audio = GetComponent<AudioSource>();
+    }
 
     private void Update()
     {
@@ -20,6 +30,8 @@ public class FireCtrl : MonoBehaviour
     void Fire()
     {
         Instantiate(bullet, firePos.position, firePos.rotation);
+
+        audio.PlayOneShot(fireSfx, 1.0f);
     }
 }
 

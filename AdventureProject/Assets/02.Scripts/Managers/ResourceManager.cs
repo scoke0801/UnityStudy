a4bb56eb7fdd7aa9c1bdafabdee6a833abf57f2d
scoke0801 <scoke0801@gmail.com>
2023-05-20 21:51:46 +0900

    [Feat]Addressable통한 리소스 로딩 추가

---
+++ AdventureProject/Assets/02.Scripts/Managers/ResourceManager.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+public class AddressableManager
+{
+    Dictionary<string, UnityEngine.Object> _resources = new Dictionary<string, UnityEngine.Object>();
+
+    public T Load<T>(string key) where T : UnityEngine.Object
+    {
+        if(_resources.TryGetValue(key, out UnityEngine.Object resources))
+        {
+            return resources as T;
+        }
+        return null;
+    }
+
+    public GameObject Instantiate(string key, Transform parent = null, bool pooling = false)
+    {
+        GameObject prefab = Load<GameObject>(key);
+        if(prefab)
+        {
+            return null;
+        }
+
+        GameObject go = UnityEngine.Object.Instantiate(prefab, parent);
+        go.name = prefab.name;
+        return go;
+    }
+
+    public void LoadAsync<T>(string key, Action<T> callback = null) where T : UnityEngine.Object
+    {
+        if(_resources.TryGetValue(key, out UnityEngine.Object resource))
+        {
+            callback?.Invoke(resource as T);
+            return;
+        }
+
+        var asyncOperation = Addressables.LoadAssetAsync<T>(key);
+        asyncOperation.Completed += (operation) =>
+        {
+            _resources.Add(key, operation.Result);
+            callback.Invoke(operation.Result);
+        };
+    }
+
+    public void LoadAllAsync<T>(string label, Action<string, int, int> callback) where T : UnityEngine.Object
+    {
+        var asyncOperation = Addressables.LoadResourceLocationsAsync(label, typeof(T));
+        asyncOperation.Completed += (operation) =>
+        {
+            int loadCount = 0;
+            int totalCount = operation.Result.Count;
+
+            foreach (var result in operation.Result)
+            {
+                ++loadCount;
+                callback?.Invoke(result.PrimaryKey, loadCount, totalCount);
+            }
+        };
+
+    }
+
+    private void Awake()
+    {
+//        Addressables.LoadAsset;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

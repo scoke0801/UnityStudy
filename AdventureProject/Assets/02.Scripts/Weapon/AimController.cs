1c6d39ed961fe6e3c3dad180e9f8d32449fa4846
scoke0801 <scoke0801@gmail.com>
2023-06-15 21:37:17 +0900

    [Fix]조준 UI위치 결정 방식 수정.
    raycast 충돌 시 충돌 지점을 위치로 설정
    충돌하지 않을 시, 카메라에서 최대 거리만큼 이동한 위치로 설정.

--- AdventureProject/Assets/02.Scripts/Weapon/AimController.cs
+++ AdventureProject/Assets/02.Scripts/Weapon/AimController.cs
@@ -1,20 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Burst.CompilerServices;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class AimController : MonoBehaviour
 {
-    [SerializeField] Camera _aimCamera;
-
+    [SerializeField] Transform _aimCamera;
+    [SerializeField] LayerMask _layerMask;
     Image _aimingUi;
-    
+
+    float _rayMaxDist = 5.0f;
+
     private void Awake()
     {
         _aimingUi = GetComponent<Image>();
         
         if( !_aimingUi) { return; }
         _aimingUi.enabled = false;
+
+        _layerMask = 0;
     }
 
     public void StartAim()
@@ -24,11 +29,26 @@ public class AimController : MonoBehaviour
         _aimingUi.enabled = true;
     }
 
+    public void FixedUpdate()
+    {
+        if (!_aimingUi) { return; }
+
+        
+    }
+
     public void LateUpdate()
     {
         if (!_aimCamera || !_aimingUi) { return; }
 
-        transform.LookAt(transform.position + _aimCamera.transform.rotation * Vector3.forward,
-            _aimCamera.transform.rotation * Vector3.up);
+        if (Physics.Raycast(_aimCamera.position, _aimCamera.TransformDirection(Vector3.forward), out RaycastHit hit, _rayMaxDist, _layerMask))
+        {
+            transform.position = hit.point;
+        }
+        else
+        {
+            transform.position = _aimCamera.position + _aimCamera.forward * _rayMaxDist;
+        }
+        transform.LookAt(transform.position + _aimCamera.rotation * Vector3.forward,
+            _aimCamera.rotation * Vector3.up);
     }
 }

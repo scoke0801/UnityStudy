16eb6b6f185b9107cbef28bcc8ec2310b683a796
scoke0801 <scoke0801@gmail.com>
2023-06-20 22:31:50 +0900

    [Test] 활 시위 당겼을 때, 활 줄의 위치가 손 위치로 가도록 수정.

--- AdventureProject/Assets/02.Scripts/Player/ArrowShooter.cs
+++ AdventureProject/Assets/02.Scripts/Player/ArrowShooter.cs
@@ -9,7 +9,12 @@ public class ArrowShooter : MonoBehaviour
     [SerializeField] float _speed = 10.0f;
     [SerializeField] private WeaponInfo _arrowWeaponInfo;
     [SerializeField] Animator _animator;
+    [SerializeField] Transform _spine;
+    [SerializeField] Quaternion _spineOffset;
+    [SerializeField] Transform _bowString;
+    [SerializeField] Transform _leftHand;
 
+    private Vector3 _bowStringOrigin;
     private Camera _aimCamera;
     private WeaponController _weaponController;
     
@@ -32,9 +37,23 @@ public class ArrowShooter : MonoBehaviour
         _weaponController = GetComponent<WeaponController>();
 
         _animator = GetComponent<Animator>();
+
+        if (_bowString)
+        {
+            _bowStringOrigin = _bowString.localPosition;
+        }
     }
     private void Update()
     {
+        if( _isAiming )
+        {
+            _bowString.position = _leftHand.position;
+        }
+        else
+        {
+            _bowString.position = _bowStringOrigin;
+        }
+
         if (!_arrow || !_isFired )
         {
             return;

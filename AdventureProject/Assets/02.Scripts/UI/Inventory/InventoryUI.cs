e7b3e6d5d4aded1e03a85131f23f5ffdb63c996d
scoke0801 <scoke0801@gmail.com>
2023-06-27 20:30:31 +0900

    [Fix] 규격 안맞던 슬롯 문제 수정.

--- AdventureProject/Assets/02.Scripts/UI/Inventory/InventoryUI.cs
+++ AdventureProject/Assets/02.Scripts/UI/Inventory/InventoryUI.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InventoryUI : MonoBehaviour
 {
@@ -55,10 +56,6 @@ public class InventoryUI : MonoBehaviour
         _connectedObject.slotUIPrefab.SetActive(false);
       
         Vector2 contentAreaSize = new Vector2(_connectedObject.contentArea.rect.width, _connectedObject.contentArea.rect.height);
-
-        Debug.Log($"ContentSizeDelta: {_connectedObject.contentArea.sizeDelta}");
-        Debug.Log($"ContentSize: {contentAreaSize}");
-
         Vector2 beginPos = new Vector2(-contentAreaSize.x * 0.5f + _option.padding, contentAreaSize.y * 0.5f -_option.padding);
         Vector2 curPos = beginPos;
 
@@ -99,4 +96,184 @@ public class InventoryUI : MonoBehaviour
         return rt;
     }
     #endregion
+
+#if UNITY_EDITOR
+    [SerializeField] private bool __showPreview = false;
+
+    [Range(0.01f, 1f)]
+    [SerializeField] private float __previewAlpha = 0.1f;
+
+    private List<GameObject> __previewSlotGoList = new List<GameObject>();
+    private int __prevSlotCountPerLine;
+    private int __prevSlotLineCount;
+    private float __prevSlotSize;
+    private float __prevSlotMargin;
+    private float __prevContentPadding;
+    private float __prevAlpha;
+    private bool __prevShow = false;
+
+    private void OnValidate()
+    {
+        if (Application.isPlaying) return;
+
+        if (__showPreview && !__prevShow)
+        {
+            CreateSlots();
+        }
+        __prevShow = __showPreview;
+
+        if (Unavailable())
+        {
+            ClearAll();
+            return;
+        }
+        if (CountChanged())
+        {
+            ClearAll();
+            CreateSlots();
+            __prevSlotCountPerLine = _option.horizontalSlotCount;
+            __prevSlotLineCount = _option.verticalSlotCount;
+        }
+        if (ValueChanged())
+        {
+            DrawGrid();
+            __prevSlotSize = _option.slotSize;
+            __prevSlotMargin = _option.slotMargin;
+            __prevContentPadding = _option.padding;
+        }
+        if (AlphaChanged())
+        {
+            SetImageAlpha();
+            __prevAlpha = __previewAlpha;
+        }
+
+        bool Unavailable()
+        {
+            return !__showPreview ||
+                    _option.horizontalSlotCount < 1 ||
+                    _option.verticalSlotCount < 1 ||
+                    _option.slotSize <= 0f ||
+                    _connectedObject.contentArea == null ||
+                    _connectedObject.slotUIPrefab == null;
+        }
+        bool CountChanged()
+        {
+            return _option.horizontalSlotCount != __prevSlotCountPerLine ||
+                   _option.verticalSlotCount != __prevSlotLineCount;
+        }
+        bool ValueChanged()
+        {
+            return _option.slotSize != __prevSlotSize ||
+                   _option.slotMargin != __prevSlotMargin ||
+                   _option.padding != __prevContentPadding;
+        }
+        bool AlphaChanged()
+        {
+            return __previewAlpha != __prevAlpha;
+        }
+        void ClearAll()
+        {
+            foreach (var go in __previewSlotGoList)
+            {
+                Destroyer.Destroy(go);
+            }
+            __previewSlotGoList.Clear();
+        }
+        void CreateSlots()
+        {
+            int count = _option.horizontalSlotCount * _option.verticalSlotCount;
+            __previewSlotGoList.Capacity = count;
+
+            for (int i = 0; i < count; i++)
+            {
+                GameObject slotGo = Instantiate(_connectedObject.slotUIPrefab);
+                slotGo.transform.SetParent(_connectedObject.contentArea);
+                slotGo.SetActive(true);
+                slotGo.AddComponent<PreviewItemSlot>();
+
+                slotGo.transform.localScale = Vector3.one;
+
+                HideGameObject(slotGo);
+
+                __previewSlotGoList.Add(slotGo);
+            }
+
+            DrawGrid();
+            SetImageAlpha();
+        }
+        void DrawGrid()
+        {
+            Vector2 contentAreaSize = new Vector2(_connectedObject.contentArea.rect.width, _connectedObject.contentArea.rect.height);
+            Vector2 beginPos = new Vector2(-contentAreaSize.x * 0.5f + _option.padding, contentAreaSize.y * 0.5f - _option.padding);
+            Vector2 curPos = beginPos;
+
+            // Draw Slots
+            int index = 0;
+            for (int j = 0; j < _option.verticalSlotCount; j++)
+            {
+                for (int i = 0; i < _option.horizontalSlotCount; i++)
+                {
+                    GameObject slotGo = __previewSlotGoList[index++];
+                    RectTransform slotRT = slotGo.GetComponent<RectTransform>();
+
+                    slotRT.anchoredPosition = curPos;
+                    slotRT.sizeDelta = new Vector2(_option.slotSize, _option.slotSize);
+                    __previewSlotGoList.Add(slotGo);
+
+                    // Next X
+                    curPos.x += (_option.slotMargin + _option.slotSize);
+                }
+
+                // Next Line
+                curPos.x = beginPos.x;
+                curPos.y -= (_option.slotMargin + _option.slotSize);
+            }
+        }
+        void HideGameObject(GameObject go)
+        {
+            go.hideFlags = HideFlags.HideAndDontSave;
+
+            Transform tr = go.transform;
+            for (int i = 0; i < tr.childCount; i++)
+            {
+                tr.GetChild(i).gameObject.hideFlags = HideFlags.HideAndDontSave;
+            }
+        }
+        void SetImageAlpha()
+        {
+            foreach (var go in __previewSlotGoList)
+            {
+                var images = go.GetComponentsInChildren<Image>();
+                foreach (var img in images)
+                {
+                    img.color = new Color(img.color.r, img.color.g, img.color.b, __previewAlpha);
+                    var outline = img.GetComponent<Outline>();
+                    if (outline)
+                        outline.effectColor = new Color(outline.effectColor.r, outline.effectColor.g, outline.effectColor.b, __previewAlpha);
+                }
+            }
+        }
+    }
+
+    private class PreviewItemSlot : MonoBehaviour { }
+
+    [UnityEditor.InitializeOnLoad]
+    private static class Destroyer
+    {
+        private static Queue<GameObject> targetQueue = new Queue<GameObject>();
+
+        static Destroyer()
+        {
+            UnityEditor.EditorApplication.update += () =>
+            {
+                for (int i = 0; targetQueue.Count > 0 && i < 100000; i++)
+                {
+                    var next = targetQueue.Dequeue();
+                    DestroyImmediate(next);
+                }
+            };
+        }
+        public static void Destroy(GameObject go) => targetQueue.Enqueue(go);
+    }
+#endif
 }

ce123b21eb36b905abf5b289e55776b135ffaffe
scoke0801 <scoke0801@gmail.com>
2023-07-04 21:51:48 +0900

    [Style] 자주 쓰는 코드 클래스 Snippet

---
+++ AdventureProject/Assets/02.Scripts/Utils/ClassSnippet.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClassSnippet : MonoBehaviour
+{
+    #region Variables
+    #endregion
+
+    #region Properties
+    #endregion
+
+    #region Unity Methods
+    #endregion
+
+    #region Public Methods
+    #endregion
+
+    #region Private Methods
+    #endregion
+}

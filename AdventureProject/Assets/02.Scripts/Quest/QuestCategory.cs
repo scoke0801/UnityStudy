c9b66878de07664869fcc400a84103fcb0622fe3
scoke0801 <scoke0801@gmail.com>
2023-07-12 22:53:13 +0900

    [Feat] 퀘스트 카테고리 스크립트 작성

--- AdventureProject/Assets/02.Scripts/Quest/QuestCategory.cs
+++ AdventureProject/Assets/02.Scripts/Quest/QuestCategory.cs
@@ -1,18 +1,69 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class QuestCategory : MonoBehaviour
+[CreateAssetMenu(menuName = "Quest/Task/Category", fileName = "QuestCategory_")]
+public class QuestCategory : ScriptableObject, IEquatable<QuestCategory>
 {
-    // Start is called before the first frame update
-    void Start()
+    #region Variables
+    [SerializeField]
+    private string _displayName;
+
+    [SerializeField]
+    private string _codeName;
+    #endregion
+
+    #region Properties
+    public string CodeName => _codeName;
+    public string DisplayName => _displayName;
+    #endregion
+
+    #region Unity Methods
+    #endregion
+
+    #region Public Methods
+    public override bool Equals(object other)
     {
-        
+        return base.Equals(other);
+    }
+    public bool Equals(QuestCategory other)
+    {
+        if (other == null)
+        {
+            return false;
+        }
+
+        if (ReferenceEquals(other, this))
+        {
+            return true;
+        }
+
+        if (GetType() != other.GetType())
+        {
+            return false;
+        }
+
+        return _codeName == other._codeName;
+    }
+    public override int GetHashCode() => (_codeName, _displayName).GetHashCode();
+
+    public static bool operator==(QuestCategory lhs, string rhs)
+    {
+        if (lhs is null)
+        {
+            return ReferenceEquals(rhs, null);
+        }
+
+        return lhs.CodeName == rhs || lhs.DisplayName == rhs;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public static bool operator!=(QuestCategory lhs, string rhs) => !(lhs == rhs);
+    #endregion
+
+    #region Protected Methods
+    #endregion
+
+    #region Private Methods
+    #endregion
 }
